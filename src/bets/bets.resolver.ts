24bef137c9aeb4aa2902d840d03b75beb211ddb2
Luis Coelho <mancoeluis@gmail.com>
2022-10-25 14:18:24 -0300

    🚧 test first authorization check

--- src/bets/bets.resolver.ts
+++ src/bets/bets.resolver.ts
@@ -1,16 +1,21 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, Context } from '@nestjs/graphql';
 import { BetsService } from './bets.service';
 import { Bet } from './entities/bet.entity';
 import { CreateBetInput } from './dto/create-bet.input';
 import { UpdateBetInput } from './dto/update-bet.input';
+import { ForbiddenException, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Resolver(() => Bet)
 export class BetsResolver {
-  constructor(private readonly betsService: BetsService) {}
+  constructor(private readonly betsService: BetsService) { }
 
   @Mutation(() => String)
-  createBets(@Args('createBetInput', { type: () => [CreateBetInput]}) createBetInput: CreateBetInput[]) {
-    return this.betsService.create(createBetInput);
+  @UseGuards(JwtAuthGuard)
+  createBets(@Args('createBetInput', { type: () => [CreateBetInput] }) createBetInput: CreateBetInput[], @Context() context) {
+    return context.req.user.roleTypes.includes('player') ? 
+          this.betsService.create(createBetInput) : 
+          new ForbiddenException('You are not authorized to perform this request.')
   }
 
   @Query(() => [Bet], { name: 'bets' })
