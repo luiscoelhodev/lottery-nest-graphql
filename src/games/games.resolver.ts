8a554ef8aecdbd0084f890f8e79fde31af1a7a22
Luis Coelho <mancoeluis@gmail.com>
2022-10-25 23:57:53 -0300

    🛂 guard games mutations and queries

--- src/games/games.resolver.ts
+++ src/games/games.resolver.ts
@@ -1,35 +1,52 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, Context } from '@nestjs/graphql';
 import { GamesService } from './games.service';
 import { Game } from './entities/game.entity';
 import { CreateGameInput } from './dto/create-game.input';
 import { UpdateGameInput } from './dto/update-game.input';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { ForbiddenException, UseGuards } from '@nestjs/common';
 
 @Resolver(() => Game)
 export class GamesResolver {
   constructor(private readonly gamesService: GamesService) {}
 
   @Mutation(() => Game)
-  createGame(@Args('createGameInput') createGameInput: CreateGameInput) {
-    return this.gamesService.create(createGameInput);
+  @UseGuards(JwtAuthGuard)
+  createGame(@Args('createGameInput') createGameInput: CreateGameInput, @Context() context) {
+    return context.req.user.roleTypes.includes('admin') ?
+           this.gamesService.create(createGameInput) :
+           new ForbiddenException('You are not authorized to perform this request.')
   }
 
   @Query(() => [Game], { name: 'games' })
-  findAll() {
-    return this.gamesService.findAll();
+  @UseGuards(JwtAuthGuard)
+  findAll(@Context() context) {
+    return context.req.user.roleTypes.includes('admin') || context.req.user.roleTypes.includes('player') ?
+           this.gamesService.findAll() :
+           new ForbiddenException('You are not authorized to perform this request.')
   }
 
   @Query(() => Game, { name: 'game' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.gamesService.findOne(id);
+  @UseGuards(JwtAuthGuard)
+  findOne(@Args('id', { type: () => Int }) id: number, @Context() context) {
+    return context.req.user.roleTypes.includes('admin') ?
+           this.gamesService.findOne(id) :
+           new ForbiddenException('You are not authorized to perform this request.')
   }
 
   @Mutation(() => Game)
-  updateGame(@Args('updateGameInput') updateGameInput: UpdateGameInput) {
-    return this.gamesService.update(updateGameInput.id, updateGameInput);
+  @UseGuards(JwtAuthGuard)
+  updateGame(@Args('updateGameInput') updateGameInput: UpdateGameInput, @Context() context) {
+    return context.req.user.roleTypes.includes('admin') ?
+           this.gamesService.update(updateGameInput.id, updateGameInput) :
+           new ForbiddenException('You are not authorized to perform this request.')
   }
 
   @Mutation(() => Game)
-  removeGame(@Args('id', { type: () => Int }) id: number) {
-    return this.gamesService.remove(id);
+  @UseGuards(JwtAuthGuard)
+  removeGame(@Args('id', { type: () => Int }) id: number, @Context() context) {
+    return context.req.user.roleTypes.includes('admin') ?
+           this.gamesService.remove(id) :
+           new ForbiddenException('You are not authorized to perform this request.')
   }
 }
