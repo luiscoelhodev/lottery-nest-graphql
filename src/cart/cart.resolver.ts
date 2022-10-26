bcb5f848e27e44ed1f559cd4d6ee1df5ef6ae553
Luis Coelho <mancoeluis@gmail.com>
2022-10-25 23:29:49 -0300

    🛂 guard cart mutations and queries

--- src/cart/cart.resolver.ts
+++ src/cart/cart.resolver.ts
@@ -1,35 +1,52 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, Context } from '@nestjs/graphql';
 import { CartService } from './cart.service';
 import { Cart } from './entities/cart.entity';
 import { CreateCartInput } from './dto/create-cart.input';
 import { UpdateCartInput } from './dto/update-cart.input';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { ForbiddenException, UseGuards } from '@nestjs/common';
 
 @Resolver(() => Cart)
 export class CartResolver {
   constructor(private readonly cartService: CartService) {}
 
   @Mutation(() => Cart)
-  createCart(@Args('createCartInput') createCartInput: CreateCartInput) {
-    return this.cartService.create(createCartInput);
+  @UseGuards(JwtAuthGuard)
+  createCart(@Args('createCartInput') createCartInput: CreateCartInput, @Context() context) {
+    return context.req.user.roleTypes.includes('admin') ?
+           this.cartService.create(createCartInput) :
+           new ForbiddenException('You are not authorized to perform this request.')
   }
 
-  @Query(() => [Cart], { name: 'cart' })
-  findAll() {
-    return this.cartService.findAll();
+  @Query(() => [Cart], { name: 'carts' })
+  @UseGuards(JwtAuthGuard)
+  findAll(@Context() context) {
+    return context.req.user.roleTypes.includes('admin') || context.req.user.roleTypes.includes('player') ?
+           this.cartService.findAll() :
+           new ForbiddenException('You are not authorized to perform this request.')
   }
 
   @Query(() => Cart, { name: 'cart' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.cartService.findOne(id);
+  @UseGuards(JwtAuthGuard)
+  findOne(@Args('id', { type: () => Int }) id: number, @Context() context) {
+    return context.req.user.roleTypes.includes('admin') || context.req.user.roleTypes.includes('player') ?
+           this.cartService.findOne(id) :
+           new ForbiddenException('You are not authorized to perform this request.')
   }
 
   @Mutation(() => Cart)
-  updateCart(@Args('updateCartInput') updateCartInput: UpdateCartInput) {
-    return this.cartService.update(updateCartInput);
+  @UseGuards(JwtAuthGuard)
+  updateCart(@Args('updateCartInput') updateCartInput: UpdateCartInput, @Context() context) {
+    return context.req.user.roleTypes.includes('admin') ?
+           this.cartService.update(updateCartInput) :
+           new ForbiddenException('You are not authorized to perform this request.')
   }
 
   @Mutation(() => Cart)
-  removeCart() {
-    return this.cartService.remove();
+  @UseGuards(JwtAuthGuard)
+  removeCart(@Context() context) {
+    return context.req.user.roleTypes.includes('admin') ?
+           this.cartService.remove() :
+           new ForbiddenException('You are not authorized to perform this request.')
   }
 }
