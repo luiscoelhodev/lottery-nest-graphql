903206eba22b61698ce12a418bfa2d0085cc8859
Luis Coelho <mancoeluis@gmail.com>
2022-10-26 00:39:52 -0300

    🐛 validate email input for login

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable, UnauthorizedException, UnprocessableEntityException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-local";
 import { AuthService } from "./auth.service";
@@ -12,6 +12,9 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(email: string, password: string) {
+    if (email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/) === null) {
+      return new UnprocessableEntityException()
+    }
     const user = await this.authService.validateUser(email, password)
     if (!user) {
       throw new UnauthorizedException()
